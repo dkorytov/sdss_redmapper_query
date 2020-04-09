29f1e0de48268e2357051c31d259ffc122043985
Dan Korytov <dkorytov@gmail.com>
2020-04-09 12:55:07 -0500

    added plot_zmr tests

--- plot_zmr.py
+++ plot_zmr.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 
 from __future__ import print_function, division
 
@@ -49,16 +49,30 @@ def plot_zmr(param_fname):
                          yerr=rad_profile_err, fmt='-o', markeredgecolor='none',
                          label=label, markerfacecolor=colors[i], color=colors[i],
                          capsize=0)
-            yerr_min = np.clip(rad_profile-rad_profile_err, a_max=np.inf, a_min=.1)
+            yerr_min = np.clip(rad_profile-rad_profile_err, a_max=np.inf, a_min=1e-3)
             plt.fill_between(r_bins_center,
                              rad_profile+rad_profile_err, yerr_min,
                              color=colors[i], alpha=0.3)
     plt.legend(loc='best', framealpha=0.0)
     plt.yscale('log')
     plt.xlabel('r/R$_{200c}$')
-    plt.ylabel('$\Sigma_{galaxy} [(R$_{200c})^{-2}$]')
+    plt.ylabel('$\Sigma_{galaxy} [(R_{200c})^{-2}$]')
     dtk.save_figs("figs/"+param_fname+"/"+__file__+"/")
 
+def check_error_zmr(param_fname):
+    zmr = load_zmr(param_fname)
+    mass_bins = zmr['m_bins'].value
+    r_bins = zmr['r_bins'].value
+    r_bins_center = dtk.bins_avg(r_bins)
+    for i in range(0, len(mass_bins)-1):
+        if zmr['zm_counts'][0][i] > 1:
+            print( "\n{:.2e} < M200 < {:.2e} ".format(mass_bins[i], mass_bins[i+1]))
+            print("counts: ", zmr['zmr_counts'][0][i][:])
+            print("density:", zmr['zmr_gal_density'][0][i][:])
+            print("error:  ", zmr['zmr_gal_density_err'][0][i][:])
+    
 if __name__ == "__main__":
-    plot_zmr(sys.argv[1])
+    param_file = sys.argv[1]
+    # plot_zmr(param_file)
+    check_error_zmr(param_file)
     plt.show()
